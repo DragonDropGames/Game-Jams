3894b050633930c579ba2c319e40f65590443355
DaikonKing <wintermight23@gmail.com>
2025-01-12 19:47:10 -0600

    added visibility variables and moved ui for points

--- Godot Jams/Jam 77/Nick/metamorphosis/enable_auto_button.gd
+++ Godot Jams/Jam 77/Nick/metamorphosis/enable_auto_button.gd
@@ -1,13 +1,9 @@
 extends Button
 
-var vis:bool = true
-var bought:int = 0;
-var clicksProvided:float = .1
-var cost:int = (35 * (bought +1))
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	visible = vis
-
+	visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Godot Jams/Jam 77/Nick/metamorphosis/node_2d.gd
+++ Godot Jams/Jam 77/Nick/metamorphosis/node_2d.gd
@@ -3,8 +3,7 @@ extends Node2D
 @onready var grow_button: Button = $Control/GrowButton
 @onready var enable_auto_button: Button = $PanelContainer/MarginContainer/ScrollContainer/UpgradeContainer/EnableAutoButton
 @onready var points_label: RichTextLabel = $Control/PointsLabel
-@onready var rich_text_label: RichTextLabel = $Control/RichTextLabel
-
+@onready var rich_text_label: RichTextLabel = $PanelContainer2/RichTextLabel
 
 
 @onready var getfirstUpgrade = get_node("PanelContainer/MarginContainer/ScrollContainer/UpgradeContainer/First Upgrade")
@@ -16,8 +15,8 @@ extends Node2D
 @onready var second_click: Button = $"PanelContainer/MarginContainer/ScrollContainer/UpgradeContainer/Second Click"
 
 var points: float = 0
-var AutoEnabled:bool = false
-var autoAmount:int = 0
+var AutoEnabled:bool = true
+var autoAmount:float = .1
 var clickAmount = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -27,12 +26,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
-	points_label.text = "[indent][color=gold][font_size=18]Leaves: " + str(points)
-	enable_auto_button.text = "Auto Click: " + str("Enabled" if AutoEnabled else "Disabled")
-	first_upgrade.text = "First Upgrade: Cost: " + str(getfirstUpgrade.cost) + "| Bought: " + str(getfirstUpgrade.bought)
-	first_click.text = "First Click Upgrade: Cost: " + str(getfirstClickUpgrade.cost) + "| Bought: " + str(getfirstClickUpgrade.bought)
-	rich_text_label.text = "[font_size=12][indent]Leaves per Second: " + str(autoAmount) +"\n Click Bonus: " + str(getfirstClickUpgrade.bought * .5)
+	var pointTotal = str(points).split(".")
+	points_label.text = "[indent][color=gold][font_size=18]Leaves: " + pointTotal[0]
+	first_upgrade.text = "First Upgrade: Cost: " + str(getfirstUpgrade.cost) + "\nBought: " + str(getfirstUpgrade.bought)
+	first_click.text = "First Click Upgrade: Cost: " + str(getfirstClickUpgrade.cost) + "\nBought: " + str(getfirstClickUpgrade.bought)
+	rich_text_label.text = "[font_size=12][indent]Leaves per Second: " + str(autoAmount) +"\nClick Bonus: " + str(getfirstClickUpgrade.bought * .5)
 
 func _on_grow_button_pressed() -> void:
 	points += clickAmount
