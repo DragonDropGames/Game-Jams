4d66ea92c84a561fb843b2d42dfbb738954c6815
Cameron Stark <cameron.stark@tierzerostudios.com>
2025-02-15 11:55:37 -0600

    wip

---
+++ Godot Jams/Jam 78/game_erase/World/world.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+var units = []
+var wagons = []
+var enemies = []
+var resources = []
+
+func _ready():
+	load_units()
+	load_enemies()
+	load_resources()
+	
+func _on_area_selected(object):
+	var start = object.start
+	var end = object.end
+	var area = []
+	
+	area.append(Vector2(min(start.x, end.x), min(start.y, end.y)))
+	area.append(Vector2(max(start.x, end.x), max(start.y, end.y)))
+	
+	var unitsInArea = get_units_in_area(area)
+	updateUnitSelectedStatus(units, false)
+	updateUnitSelectedStatus(wagons, false)
+	updateUnitSelectedStatus(unitsInArea, true)
+
+func updateUnitSelectedStatus(items, isSelected):
+	for item in items:
+		item.setSelected(isSelected)
+
+func get_units_in_area(area):
+	var unitsInArea = []
+	for unit in units:
+		if (unit.position.x > area[0].x) and (unit.position.x < area[1].x):
+			if (unit.position.y > area[0].y) and (unit.position.y < area[1].y):
+				unitsInArea.append(unit)
+	
+	for wagon in wagons:
+		if (wagon.position.x > area[0].x) and (wagon.position.x < area[1].x):
+			if (wagon.position.y > area[0].y) and (wagon.position.y < area[1].y):
+				unitsInArea.append(wagon)
+	
+	return unitsInArea
+
+func load_units():
+	units = null
+	units = get_tree().get_nodes_in_group("Units")
+	wagons = get_tree().get_nodes_in_group("Wagons")
+	
+func load_enemies():
+	enemies = null
+	enemies = get_tree().get_nodes_in_group("Enemies")
+	
+func load_resources():
+	resources = null
+	resources = get_tree().get_nodes_in_group("Resources")
