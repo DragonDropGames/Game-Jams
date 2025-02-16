c20b1115449ddc78891beb6f07c66f5396983a73
Cameron Stark <cameron.stark@tierzerostudios.com>
2025-02-16 15:42:20 -0600

    fix movement and add fog updating on wagon position

--- Godot Jams/Jam 78/game_erase/Units/wagon.gd
+++ Godot Jams/Jam 78/game_erase/Units/wagon.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
 # All Wagon Properties
-var speed = 10
+var speed = 20
 var followCursor = false
 @export var isSelected = false
 @onready var target = position
 @onready var selectedPanel = get_node("SelectedPanel")
 
 # Wagon Customization Properties
-@onready var bowWagonImage = get_node("CollisionShape2D/BowWagon")
-@onready var mainWagonImage = get_node("CollisionShape2D/MainWagon")
-@onready var resourceWagonImage = get_node("CollisionShape2D/ResourceWagon")
-@onready var swordWagonImage = get_node("CollisionShape2D/SwordWagon")
+@onready var bowWagonImage = get_node("BowWagon")
+@onready var mainWagonImage = get_node("MainWagon")
+@onready var resourceWagonImage = get_node("ResourceWagon")
+@onready var swordWagonImage = get_node("SwordWagon")
 @export var isMainWagon = false
 @export var isSwordWagon = false
 @export var isBowWagon = false
@@ -25,7 +25,7 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("RightClick"):
 		followCursor = true
-	elif event.is_action_pressed("RightClick"):
+	elif event.is_action_released("RightClick"):
 		followCursor = false
 		
 func _physics_process(delta: float) -> void:
@@ -41,21 +41,24 @@ func setSelected(value):
 	isSelected = value
 	selectedPanel.visible = value
 	
-
 func setProperties():
 	if isMainWagon:
-		speed = 5
+		speed = 15
 		mainWagonImage.visible = true
 		lightRadius = 30
+		add_to_group("MainWagon", true)
 	elif isSwordWagon:
-		speed = 10
+		speed = 20
 		swordWagonImage.visible = true
 		lightRadius = 15
+		add_to_group("SwordWagon", true)
 	elif isBowWagon:
-		speed = 10
+		speed = 20
 		bowWagonImage.visible = true
 		lightRadius = 15
+		add_to_group("BowWagon", true)
 	elif isResourceWagon:
 		speed = 15
 		resourceWagonImage.visible = true
 		lightRadius = 20
+		add_to_group("ResourceWagon", true)
