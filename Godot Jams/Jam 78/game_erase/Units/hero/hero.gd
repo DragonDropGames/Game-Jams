5ab3e6ae989a57a5e4cd432f81a1277d5ebd9372
DaikonKing <wintermight23@gmail.com>
2025-02-19 23:40:46 -0600

    hero with health and light that doesn't deplete

---
+++ Godot Jams/Jam 78/game_erase/Units/hero/hero.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+var speed = 20
+var maxHealth = 1200
+
+var followCursor = false
+@export var isSelected = false
+@onready var target = position
+@onready var selectedPanel = get_node("SelectedPanel")
+@onready var healthBar = get_node("BasicHealthBar")
+@onready var hplabel = $Label
+var lightScale: Vector2 = Vector2(10, 10)
+var light_depletion_rate: int = 0
+
+func _ready():
+	add_to_group("Wagons", true)
+	healthBar.max_value = maxHealth
+	hplabel.text= str(healthBar.max_value)
+	
+func _process(delta: float) -> void:
+	
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("RightClick"):
+		followCursor = true
+	elif event.is_action_released("RightClick"):
+		followCursor = false
+		
+func _physics_process(delta: float) -> void:
+	if followCursor && isSelected:
+		target = get_global_mouse_position()
+	
+	velocity = position.direction_to(target) * speed
+	
+	if position.distance_to(target) > 10:
+		move_and_slide()
+
+func setSelected(value):
+	isSelected = value
+	selectedPanel.visible = value
