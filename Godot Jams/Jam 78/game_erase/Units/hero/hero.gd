1ae4ae40ead49a64754712157fb59d4aff96ae5f
Cameron Stark <cameron.stark@tierzerostudios.com>
2025-02-23 14:27:27 -0600

    add missing selection on hero

--- Godot Jams/Jam 78/game_erase/Units/hero/hero.gd
+++ Godot Jams/Jam 78/game_erase/Units/hero/hero.gd
@@ -2,6 +2,7 @@ extends ControlableUnit
 
 @onready var heroSprite: AnimatedSprite2D = $CollisionShape2D/Hero
 @onready var main_health_bar: TextureProgressBar = get_tree().get_root().get_node("World/UI/HeroHealthBar")
+var mouseEntered = false
 
 func _ready():
 	is_hero = true
@@ -24,3 +25,16 @@ func _process(delta: float) -> void:
 	else:
 		main_health_bar.value = health
 		
+func _on_interaction_panel_mouse_entered() -> void:
+	mouseEntered = true
+
+func _on_interaction_panel_mouse_exited() -> void:
+	mouseEntered = false
+
+func _on_interaction_panel_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("LeftClick") and alive:
+		if mouseEntered:
+			if not is_selected:
+				set_selected(true)
+			else:
+				set_selected(false)
