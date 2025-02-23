233311b834c5c32d672202279900e615f3e36186
Drake Anglin <Drake.Anglin@aa.com>
2025-02-22 19:56:49 -0600

    minor fixes

--- Godot Jams/Jam 78/game_erase/Global/controlable_unit.gd
+++ Godot Jams/Jam 78/game_erase/Global/controlable_unit.gd
@@ -114,10 +114,11 @@ func _input(event):
 		follow_cursor = false
 
 func _on_light_timer() -> void:
-	if light_depletion_rate > 0:
-		Game.consumeWood()
-		light_scale -= light_depletion_rate
-		scale_lights()
+	if light_depletion_rate > 0.0:
+		var recieved_wood = Game.consumeWood()
+		if not recieved_wood:
+			light_scale -= light_depletion_rate
+			scale_lights()
 
 func _on_light_area_entered(body: Node2D) -> void:
 	if body.is_in_group(group_name):
@@ -138,9 +139,13 @@ func _on_health_check_timer_timeout() -> void:
 func scale_lights() -> void:
 	if !light_collision:
 		return
-		
-	light_collision.scale = Vector2(light_scale, light_scale)
-	point_light.scale = Vector2(light_scale, light_scale)
+	
+	if light_scale > 0:
+		light_collision.scale = Vector2.ONE * light_scale
+		point_light.scale = Vector2.ONE * light_scale
+	else:
+		light_collision.scale = Vector2.ZERO
+		point_light.scale = Vector2.ZERO
 
 func update_sprit(name: String) -> void:
 	if sprite:
