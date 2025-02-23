a995a65d03f2b6f8408f64d903ff2702421c7ba2
Drake Anglin <Drake.Anglin@aa.com>
2025-02-22 21:35:59 -0600

    fixed merge issues

--- Godot Jams/Jam 78/game_erase/Units/Resource Collectors/resourceUnit.gd
+++ Godot Jams/Jam 78/game_erase/Units/Resource Collectors/resourceUnit.gd
@@ -32,7 +32,7 @@ func _on_gather_range_body_entered(body: Node2D) -> void:
 func _on_gather_range_body_exited(body: Node2D) -> void:
 	if body.is_in_group('Resource'):
 		gathering_resources = false
-    sprite.play('idle')
+		sprite.play('idle')
 
 func get_closest_tree_tile() -> Vector2:
 	var player_tile: Vector2 = tile_map.local_to_map(position)
