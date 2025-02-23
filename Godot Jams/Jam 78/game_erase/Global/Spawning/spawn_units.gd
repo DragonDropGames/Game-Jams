008f6085f65ee3c2e2083f33a130a2d2f63566c7
Cameron Stark <cameron.stark@tierzerostudios.com>
2025-02-22 21:04:22 -0600

    fix unit wagon spawning

--- Godot Jams/Jam 78/game_erase/Global/Spawning/spawn_units.gd
+++ Godot Jams/Jam 78/game_erase/Global/Spawning/spawn_units.gd
@@ -5,19 +5,18 @@ var enemyUnit = preload("res://Units/enemies/enemy.tscn")
 
 
 func spawn(type: Enums.UNIT_TYPE, position: Vector2) -> void:
-	var path = get_tree().get_root().get_node("World/WorldNodes/Units")
+	var path = get_tree().get_root().get_node("World/Characters")
 	var worldPath = get_tree().get_root().get_node("World")
 	var unit = combatUnit.instantiate()
 	
 	unit.unit = type
-	unit.position = position
-	unit._ready()
+	unit.position = position + Vector2(randi_range(-25, 25), randi_range(-25, 25))
 	path.add_child(unit)
 	worldPath.load_units()
 
 
 func spawnEnemy(type: Enums.ENEMY_TYPE, position: Vector2) -> void:
-	var enemies_path = get_tree().get_root().get_node_or_null("World/WorldNodes/Enemies")
+	var enemies_path = get_tree().get_root().get_node_or_null("World/Enemies")
 	var world = get_tree().get_root().get_node_or_null("World")
 
 	# Ensure the path exists
