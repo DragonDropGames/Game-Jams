986f481f17ecf6cb11ea62143a88b36e096767a8
DaikonKing <wintermight23@gmail.com>
2025-02-22 18:52:58 -0600

    main screene

--- Godot Jams/Jam 78/game_erase/main_menu.gd
+++ Godot Jams/Jam 78/game_erase/main_menu.gd
@@ -4,3 +4,8 @@ extends Control
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://World/world.tscn")
 	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
