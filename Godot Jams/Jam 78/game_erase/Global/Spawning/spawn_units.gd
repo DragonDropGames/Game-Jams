12ccfbf92d52cc138587525f60cc122107f658fc
Drake Anglin <Drake.Anglin@aa.com>
2025-02-23 11:17:19 -0600

    close #139

--- Godot Jams/Jam 78/game_erase/Global/Spawning/spawn_units.gd
+++ Godot Jams/Jam 78/game_erase/Global/Spawning/spawn_units.gd
@@ -4,7 +4,6 @@ var combatUnit = preload("res://Units/Combat/combatUnit.tscn")
 var resourceUnit = preload("res://Units/Resource Collectors/resourceUnit.tscn")
 var enemyUnit = load("res://Units/enemies/enemy.tscn")
 
-
 func spawn(type: Enums.UNIT_TYPE, position: Vector2) -> void:
 	var path = get_tree().get_root().get_node("World/Characters")
 	var worldPath = get_tree().get_root().get_node("World")
@@ -24,7 +23,7 @@ func spawn(type: Enums.UNIT_TYPE, position: Vector2) -> void:
 func spawnEnemy(type: Enums.ENEMY_TYPE, position: Vector2) -> void:
 	var enemies_path = get_tree().get_root().get_node_or_null("World/Enemeis")
 	var world = get_tree().get_root().get_node_or_null("World")
-
+	
 	var unit = enemyUnit.instantiate()
 	unit.enemy = type
 	unit.position = position
