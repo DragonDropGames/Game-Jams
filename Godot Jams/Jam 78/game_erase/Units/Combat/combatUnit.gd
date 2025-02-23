4a9aaae1e87ee3a298333845327b36bd16106f91
Drake Anglin <Drake.Anglin@aa.com>
2025-02-23 10:44:19 -0600

    restored bows

--- Godot Jams/Jam 78/game_erase/Units/Combat/combatUnit.gd
+++ Godot Jams/Jam 78/game_erase/Units/Combat/combatUnit.gd
@@ -6,30 +6,24 @@ extends ControlableUnit
 @onready var bow_image: AnimatedSprite2D = $Collision/BowUnit
 
 func _ready():
-	light_scale = 5
-	health = 100
 	add_to_group("Units")
 	
+	light_scale = 5
+	health = 100
 	speed = 38
-	attack_area = $SwordArea
-	attack_damage = 10
-	attack_frequency = 1
-	sprite = sword_image
-	sword_image.visible = true
 
-
-	#match unit: 
-		#Enums.UNIT_TYPE.SWORD:
-			#attack_area = $SwordArea
-			#attack_damage = 10
-			#attack_frequency = 1
-			#sprite = sword_image
-			#sword_image.visible = true
-		#Enums.UNIT_TYPE.BOW:
-			#attack_area = $BowArea
-			#attack_damage = 25
-			#attack_frequency = 5
-			#sprite = bow_image
-			#bow_image.visible = true
+	match unit: 
+		Enums.UNIT_TYPE.SWORD:
+			attack_area = $SwordArea
+			attack_damage = 10
+			attack_frequency = 1
+			sprite = $Collision/SwordUnit
+		Enums.UNIT_TYPE.BOW:
+			attack_area = $BowArea
+			attack_damage = 25
+			attack_frequency = 5
+			sprite = $Collision/BowUnit
+	
+	sprite.visible = true
 	
 	ready_complete()
